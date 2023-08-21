09579f04a50e14c8f2f1538c2f25381e3de89b51
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-08-21 20:42:37 -0300

    Add files via upload

---
+++ PI-semestre02/exemplojanelas/src/main/java/com/mycompany/exemplojanelas/Exemplojanelas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplojanelas;
+
+/**
+ *
+ * @author janaina.csilva64
+ */
+public class Exemplojanelas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
