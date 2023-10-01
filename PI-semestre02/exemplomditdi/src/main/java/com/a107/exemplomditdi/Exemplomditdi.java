2d11875735f7daf1da66a97da05dea095a4fae6d
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-10-01 15:39:12 -0300

    Add files via upload

---
+++ PI-semestre02/exemplomditdi/src/main/java/com/a107/exemplomditdi/Exemplomditdi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.a107.exemplomditdi;
+
+/**
+ *
+ * @author fernando.tfernandes
+ */
+public class Exemplomditdi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
