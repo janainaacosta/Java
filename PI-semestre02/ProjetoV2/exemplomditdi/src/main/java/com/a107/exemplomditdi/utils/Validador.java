93d08c5e18de99f87d3b10af3156b2c42768a1be
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-10-02 17:38:19 -0300

    Add files via upload

---
+++ PI-semestre02/ProjetoV2/exemplomditdi/src/main/java/com/a107/exemplomditdi/utils/Validador.java
@@ -0,0 +1,146 @@
+package com.a107.exemplomditdi.utils;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author fernando.tfernandes
+ */
+public class Validador {
+     public ArrayList<String> mensagensErro = new ArrayList<>();
+    
+    /**
+     * Valida somente campos inteiros
+     * @param txt Recebe um objeto do tipo JTextField
+     */
+    public void ValidarNumero(JTextField txt){
+    
+        try{
+            
+            //Verifico se o campo está vazio
+            if(txt.getText().trim().equals("")){
+                throw new IllegalArgumentException();
+            }
+            
+            int valorConvertido = Integer.parseInt(txt.getText());
+            txt.setBackground(Color.WHITE);
+            
+        }catch(NumberFormatException e){
+        
+            this.mensagensErro.add("Falha ao converter o valor do campo " + txt.getName() + " em inteiro");
+            txt.setBackground(Color.red);
+        }catch(IllegalArgumentException e){
+            this.mensagensErro.add("Digite um valor para o campo " + txt.getName());
+            txt.setBackground(Color.red);
+        }
+    
+    }
+    
+    
+    public void ValidarTexto(JTextField txt){
+    
+        try{
+            
+            //Verifico se o campo está vazio
+            if(txt.getText().replace(".", "")
+                            .replace("-", "")
+                            .trim()
+                            .equals("")){
+                throw new IllegalArgumentException();
+            }
+            
+            txt.setBackground(Color.WHITE);
+            
+        }catch(IllegalArgumentException e){
+            this.mensagensErro.add("Digite um valor para o campo " + txt.getName());
+            txt.setBackground(Color.red);
+        }
+    
+    }
+    
+    
+    
+    
+    
+    /**
+     * Valida somente campos float
+     * @param txt Recebe um objeto do tipo JTextField
+     */
+     public void ValidarFloat(JTextField txt){
+     
+         try{
+            
+            //Verifico se o campo está vazio
+            if(txt.getText().trim().equals("")){
+                throw new IllegalArgumentException();
+            }
+            
+            float valorConvertido = Float.parseFloat(txt.getText());
+            txt.setBackground(Color.WHITE);
+            
+        }catch(NumberFormatException e){
+        
+            this.mensagensErro.add("Falha ao converter o valor do campo " + txt.getName() + " em float");
+            txt.setBackground(Color.red);
+        }catch(IllegalArgumentException e){
+            this.mensagensErro.add("Digite um valor para o campo " + txt.getName());
+            txt.setBackground(Color.red);
+        }
+     
+     }
+     
+     public void limparMensagens(){
+     
+         this.mensagensErro.clear();
+     }
+     
+     /**@deprecated substituida por {@link #getMensagensErro()}
+      * Método para exibir mensagens de erro na tela com JOptionPane
+      */
+     public void ExibirMensagensErro(){
+         
+        String errosFormulario = "";
+        for (String msg : this.mensagensErro) {
+            errosFormulario += msg + "\n";
+        }
+        
+        if(!errosFormulario.equals("")){
+            JOptionPane.showMessageDialog(null, errosFormulario);
+            this.limparMensagens();
+        }     
+
+     }
+     
+     /**
+      * Resgata todos os erros gerados em uma única String
+      * @return 
+      */
+     public String getMensagensErro(){
+         
+        String errosFormulario = "";
+        
+        //Percorro o arrayList e concateno na variável errosFormulario
+        for (String msg : this.mensagensErro) {
+            errosFormulario += msg + "\n";
+        }
+        
+        if(!errosFormulario.equals("")){
+            this.limparMensagens();
+        }     
+        
+        return errosFormulario;
+        
+     }
+     
+     public boolean hasErro(){
+     
+         if(this.mensagensErro.size()>0){
+            return true;    
+         }else{
+             return false;
+         }
+     }
+}
