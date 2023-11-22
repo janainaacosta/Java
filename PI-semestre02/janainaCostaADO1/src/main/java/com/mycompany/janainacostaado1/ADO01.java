935b7b40bc2ea1a397c9aeee29a2d5155f8243a3
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-11-22 12:46:53 -0300

    Add files via upload

---
+++ PI-semestre02/janainaCostaADO1/src/main/java/com/mycompany/janainacostaado1/ADO01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.janainacostaado1;
+
+/**
+ *
+ * @author Janaina Costa
+ */
+public class ADO01 {
+    
+    public static void main(String[] args) {
+        Animal a1 = new Animal();
+        a1.setNome("joaninha ninha");
+        a1.setCor("amarela");
+        a1.setPintas(true);
+        a1.setQtde_pintas(11);
+    }
+    
+}
